06db412150f8b308679c754e0b636ab15f45e06d
MMike17 <matthews.r.mike@gmail.com>
2025-10-22 11:39:36 +0200

    Add general settings

---
+++ Assets/Scripts/Data/GeneralSettings.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+/// <summary>Settings for general use</summary>
+[CreateAssetMenu(fileName = nameof(GeneralSettings), menuName = "Data/" + nameof(GeneralSettings))]
+public class GeneralSettings : StaticScriptableObject<GeneralSettings>
+{
+#if UNITY_EDITOR
+	[MenuItem("Tools/" + nameof(GeneralSettings))]
+	static void Select() => Selection.activeObject = Get();
+#endif
+
+	// [Header(nameof())]
+}

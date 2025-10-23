3876e06bf98968df82dacf436099d16a2c88c6e3
MMike17 <matthews.r.mike@gmail.com>
2025-10-23 18:52:40 +0200

    Fix window resizing

--- Assets/Scripts/Engine/Panel.cs
+++ Assets/Scripts/Engine/Panel.cs
@@ -9,8 +9,32 @@ public class Panel : MonoBehaviour
 	public bool overrides;
 	public bool fullScreen;
 
+	private RectTransform rectTransform;
+	private Vector2Int size;
+
+	private void Init()
+	{
+		if (rectTransform != null)
+			return;
+
+		rectTransform = GetComponent<RectTransform>();
+
+		if (overrides)
+		{
+			if (fullScreen)
+			{
+				Resolution res = Screen.resolutions[^1];
+				size = new Vector2Int(res.width, res.height);
+			}
+			else
+				size = new Vector2Int(Mathf.FloorToInt(rectTransform.rect.width), Mathf.FloorToInt(rectTransform.rect.height));
+		}
+	}
+
 	public virtual void Pop()
 	{
+		Init();
+
 		if (fullScreen && !overrides)
 			overrides = true;
 
@@ -19,20 +43,10 @@ public class Panel : MonoBehaviour
 			if (current != null)
 				current.gameObject.SetActive(false);
 
-			if (fullScreen)
-			{
-				Resolution res = Screen.resolutions[^1];
-				Screen.SetResolution(res.width, res.height, true);
-			}
-			else
-			{
-				RectTransform rectTransform = GetComponent<RectTransform>();
-				Screen.SetResolution(
-					Mathf.FloorToInt(rectTransform.rect.width),
-					Mathf.FloorToInt(rectTransform.rect.width),
-					false
-				);
+			Screen.SetResolution(size.x, size.y, false);
 
+			if (!fullScreen)
+			{
 				rectTransform.anchorMin = Vector2.zero;
 				rectTransform.anchorMax = Vector2.one;
 				rectTransform.sizeDelta = Vector2.zero;
